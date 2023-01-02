2efd9b411e0b92bc70521f935e65fce2d9784727
Woong <rlqmatk@naver.com>
2023-01-02 15:00:19 +0900

    Done: Section05

--- lazy-days/client/src/components/appointments/hooks/monthYear.ts
+++ lazy-days/client/src/components/appointments/hooks/monthYear.ts
@@ -1,25 +1,21 @@
 import dayjs from 'dayjs';
 
-// for storing current month / year details
 export interface MonthYear {
-  startDate: dayjs.Dayjs; // first day of the month
-  firstDOW: number; // day of week; 0 === Sunday
-  lastDate: number; // last date of the month
-  monthName: string; // name of the month
-  month: string; // two digit month number
-  year: string; // four digit year
+  startDate: dayjs.Dayjs;
+  firstDOW: number;
+  lastDate: number;
+  monthName: string;
+  month: string;
+  year: string;
 }
 
-// for incrementing MonthYear
 export function getUpdatedMonthYear(
   monthYear: MonthYear,
   monthIncrement: number,
 ): dayjs.Dayjs {
-  // the clone is necessary to prevent mutation
   return monthYear.startDate.clone().add(monthIncrement, 'months');
 }
 
-// get calendar-relevant data for the month containing initialDate
 export function getMonthYearDetails(initialDate: dayjs.Dayjs): MonthYear {
   const month = initialDate.format('MM');
   const year = initialDate.format('YYYY');
@@ -34,9 +30,7 @@ export function getNewMonthYear(
   prevData: MonthYear,
   monthIncrement: number,
 ): MonthYear {
-  // update the monthYear by the specified increment
   const newMonthYear = getUpdatedMonthYear(prevData, monthIncrement);
 
-  // return object with the details for the new monthYear
   return getMonthYearDetails(newMonthYear);
 }
